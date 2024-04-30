f2b87a9ef43aa048c9c0d1a16a5d1210751d21e9
JeanCarlos <jmatochepascual@gmail.com>
2024-04-29 21:48:24 -0500

    [FEATURE] listar tweets de localstorage

--- js/app.js
+++ js/app.js
@@ -8,11 +8,31 @@ let tweetsLocal = [];
 
 
 // Funciones
+// Obtener tweets del local storage
 const obtenerTweetsStorage = () => {
   const tweetsStorage = localStorage.getItem('tweets') || [];
+
+  listarTweets(tweetsStorage);
 };
 
 
+
+
+const listarTweets = (tweetsStorage) => {
+
+  tweetsStorage.forEach((tweet) => {
+    const botonEliminar = document.createElement('p');
+    botonEliminar.classList.add('borrar-tweet');
+    botonEliminar.innerText = 'X';
+
+    const li = document.createElement('li');
+    li.innerText = tweet;
+    li.appendChild(botonEliminar);
+
+    listaTweetElement.appendChild(li);
+  });
+
+}
 
 
 
