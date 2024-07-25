168ebda0a9758c93e88c0b0467f15368f271a389
JeanCarlos <jmatochepascual@gmail.com>
2024-07-24 21:38:07 -0500

    [FEATURE] Mostrar mensaje de error

--- js/app.js
+++ js/app.js
@@ -3,7 +3,10 @@ import {
   tweetInputElement,
   formularioElement
 } from './elemetos.js';
-import { validarTweet } from './funciones.js';
+import {
+  mostrarAlerta,
+  validarTweet
+} from './funciones.js';
 
 // Funcion que inicializa la aplicacion
 function init(e) {
@@ -15,8 +18,16 @@ function init(e) {
   // Verificar tweet
   const esTweetValido = validarTweet(tituloTweet);
 
+
+  // Mostrar mensaje de error si el tweet no es valido
+  if (!esTweetValido) {
+    mostrarAlerta('El tweet no puede estar vacio');
+    return;
+  }
 }
 
+
+// Eventos
 document.addEventListener('DOMContentLoaded', () => {
   formularioElement.addEventListener('submit', init);
 });
--- js/funciones.js
+++ js/funciones.js
@@ -1,7 +1,33 @@
+import { formularioElement } from "./elemetos.js";
+
+
 // Verificar tweet
 const validarTweet = (titulo) => titulo !== '' ? true : false;
 
 
+// Mostrar mensaje de error
+const mostrarAlerta = (mensaje) => {
+
+  // Verificar si ya existe un mensaje de error
+  const existemMensajeDeError = document.querySelector('.error');
+  if (existemMensajeDeError) return;
+
+  // Crear mensaje de error
+  const mensajeError = document.createElement('p');
+  mensajeError.textContent = mensaje;
+  mensajeError.classList.add('error');
+
+  // Insertar mensaje de error
+  formularioElement.appendChild(mensajeError);
+
+  // Eliminar mensaje de error
+  setTimeout(() => {
+    mensajeError.remove();
+  }, 3000);
+}
+
+
 export {
-  validarTweet
+  validarTweet,
+  mostrarAlerta
 }
