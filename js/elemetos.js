930c1158c69f16eea648bcc0f1caff77c34dbcfd
JeanCarlos <jmatochepascual@gmail.com>
2024-07-24 21:10:00 -0500

    [ADD] separar elementos del dom

---
+++ js/elemetos.js
@@ -0,0 +1,9 @@
+const tweetInputElement = document.querySelector('#tweet');
+const listaTweetElement = document.querySelector('#lista-tweets');
+const formularioElement = document.querySelector('#formulario');
+
+export {
+  tweetInputElement,
+  listaTweetElement,
+  formularioElement
+}
